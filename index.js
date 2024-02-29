7d14aec3e22e6a1e97cc3a92db47c45c03aa7ca0
cameron otoole <cameronotoole72@gmail.com>
2024-02-29 16:32:17 -0500

    added userSelection vs. compSelection

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let userSelection = prompt("rock, paper or scissors?"); //Gets the users selecti
 let computerSelection = Math.random(); //Randomly generate a number
 
 //Conditional logic goes here to determine the computers selection
-if (computerSelection < 0.34) {
+if (computerSelection < ) {
     computerSelection = "rock";
-} else if (computerSelection <= 0.67) {
+} else if (computerSelection <= ) {
     computerSelection = "paper";
 } else {
     computerSelection = "scissors";
@@ -16,6 +16,36 @@ if (computerSelection < 0.34) {
 
 //Function to compare the userSelection and the computerSelection to determine the winner
 let compare = function (selection1, selection2) {
+    if (selection1 === selection2) {
+        return "It's a tie!";
+    } else if (selection1 === "rock") {
+        if (selection2 === "scissors") {
+            return "rock wins!";
+        }
+        else if (selection2 === "paper") {
+            return "paper wins!";
+        }
+    }
+    else if (selection1 === "paper") {
+        if (selection2 === "scissors") {
+            return "scissors win!"
+        }
+        else if (selection2 === "rock") {
+            return "paper wins!";
+        }
+    }
+    else if (selection1 === "scissors") {
+        if (selection2 === "paper") {
+            return "scissors win"
+        }
+        else if (selection2 === "rock") {
+            return "rock wins!";
+        }
+    }
 
 }
+
+
+
+
 console.log(compare(userSelection, computerSelection)); //call the function to compare and display result
