e788a86f85a939d1ef8b997ce1fcff0470756586
cameron otoole <cameronotoole72@gmail.com>
2024-02-29 15:44:52 -0500

    added computer conditionals

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // rock paper scissors JS activity
 
 let userSelection = prompt("rock, paper or scissors?"); //Gets the users selection
-let computerSelection = Math.random(); //Randomly generate a number                        
+let computerSelection = Math.random(); //Randomly generate a number
 
 //Conditional logic goes here to determine the computers selection
+if (computerSelection < 0.34) {
+    computerSelection = "rock";
+} else if (computerSelection <= 0.67) {
+    computerSelection = "paper";
+} else {
+    computerSelection = "scissors";
+}
 
 //<--Code goes here-->
 
